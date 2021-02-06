ab1046cd078ec2eb5d19d97f8fc53b074fb3c958
flag2020 <2918434886@qq.com>
2021-02-06 17:25:59 +0800

    Site updated: 2021-02-06 17:25:56

---
+++ test.php
@@ -0,0 +1,15 @@
+<?php
+$a = chr(112).chr(104).chr(112).chr(105).chr(110).chr(102).chr(111);
+$a();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>test</title>
+	</head>
+	<body>
+		test
+		<a href="http://39.107.126.173:8320/uploads/picture/20210206/58008f85db56489fefbca7db668542c4.jpg">aaa</a>
+	</body>
+</html>
